8453b6926c7edd5a949e0cd4bbbc15c93681fed1
ftrbnd <giosalas25@gmail.com>
2024-09-10 14:42:48 -0700

    Add getDownloadLink function

--- internal/gmail/gmail.go
+++ internal/gmail/gmail.go
@@ -1,8 +1,10 @@
 package gmail
 
 import (
+	"encoding/base64"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/ftrbnd/film-sync/internal/util"
@@ -10,8 +12,8 @@ import (
 )
 
 func getEmailsBySender(sender string, service *gmail.Service) []*gmail.Message {
-	
 	q := fmt.Sprintf("from:%s", sender)
+
 	res, err := service.Users.Messages.List("me").Q(q).Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve messages from %s: %v", sender, err)
@@ -41,6 +43,24 @@ func filterEmailsByMetadata(messages []*gmail.Message, fieldName string, fieldVa
 	return emails
 }
 
+func getDownloadLink(message *gmail.Message, service *gmail.Service) string {
+	msg, err := service.Users.Messages.Get("me", message.Id).Format("full").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve message: %v", err)
+	}
+
+	data := msg.Payload.Parts[0].Body.Data
+	decoded, err := base64.URLEncoding.DecodeString(data)
+	if err != nil {
+		log.Fatalf("Unable to decode message body: %v", err)
+	}
+
+	lines := strings.Split(string(decoded), "\n")
+	link := lines[6] // or find by index of https://wetransfter.com/downloads
+
+	return link
+}
+
 func checkEmail(t time.Time) {
 	fmt.Println(t, "Checking email...")
 
@@ -50,8 +70,11 @@ func checkEmail(t time.Time) {
 	
 	e := getEmailsBySender(fromEmail, service)
 	f := filterEmailsByMetadata(e, "Reply-To", replyToEmail, service)
-	
-	log.Default().Printf("%d emails found", len(f))
+
+	for _, email := range(f) {
+		link := getDownloadLink(email, service)
+		log.Default().Println(link)
+	}
 }
 
 func ScheduleJob() {

abf3a8cddc09cdaaf697293186e8f310df155a58
ftrbnd <giosalas25@gmail.com>
2024-09-26 15:06:24 -0700

    Set up interactions for setting folder name

--- internal/discord/discord.go
+++ internal/discord/discord.go
@@ -1,7 +1,9 @@
 package discord
 
 import (
+	"fmt"
 	"log"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/ftrbnd/film-sync/internal/util"
@@ -13,15 +15,68 @@ func Session() *discordgo.Session {
 	s, err := discordgo.New("Bot " + token)
 	util.CheckError("Unable to start Discord session", err)
 
+	s.AddHandler(handleInteractionCreate)
+	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Default().Printf("[Discord] %s is ready", s.State.User)
+	})
+
 	err = s.Open()
 	util.CheckError("Failed to open Discord session", err)
 
-	s.AddHandler(func(s *discordgo.Session, m *discordgo.InteractionCreate) {
-		log.Default().Println("EVENT:", m.Type)
-	})
-
-	log.Default().Printf("[Discord] %s is ready", s.State.User)
 	return s
+}
+
+func handleInteractionCreate(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	switch i.Type {
+	case discordgo.InteractionMessageComponent:
+		buttonID := i.MessageComponentData().CustomID
+
+		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseModal,
+			Data: &discordgo.InteractionResponseData{
+				CustomID: "folder_name_modal_" + buttonID,
+				Title:    "Set the folder name",
+				Components: []discordgo.MessageComponent{
+					discordgo.ActionsRow{
+						Components: []discordgo.MessageComponent{
+							discordgo.TextInput{
+								CustomID:    "folder_name_input_" + buttonID,
+								Label:       "Enter the folder name",
+								Style:       discordgo.TextInputShort,
+								Placeholder: "May 2024",
+								Required:    true,
+								MaxLength:   20,
+								MinLength:   1,
+							},
+						},
+					},
+				},
+			},
+		})
+		util.CheckError("Failed to send modal", err)
+	case discordgo.InteractionModalSubmit:
+		data := i.ModalSubmitData()
+		folderName := data.Components[0].(*discordgo.ActionsRow).Components[0].(*discordgo.TextInput).Value
+
+		after, _ := strings.CutPrefix(data.CustomID, "folder_name_modal_")
+		urls := strings.Split(after, ",")
+		// TODO: set folder names
+		log.Default().Println(urls)
+
+		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Embeds: []*discordgo.MessageEmbed{
+					{
+						Title:       "Folder name set!",
+						Description: folderName,
+						Color:       0x32FF25,
+					},
+				},
+			},
+		})
+		util.CheckError("Failed to respond to modal submission", err)
+	}
 }
 
 func createDMChannel(s *discordgo.Session) *discordgo.Channel {
@@ -84,6 +139,14 @@ func SendSuccessMessage(s3Url string, driveUrl string, message string, s *discor
 						Label: "AWS S3",
 						Style: discordgo.LinkButton,
 						URL:   s3Url,
+					},
+					discordgo.Button{
+						Label:    "Set folder name",
+						Style:    discordgo.PrimaryButton,
+						CustomID: fmt.Sprintf("%s,%s", s3Url, driveUrl),
+						Emoji: &discordgo.ComponentEmoji{
+							Name: "📁",
+						},
 					},
 				},
 			},
